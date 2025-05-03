de59486d6664abbe28d05a3d1de180cdd9524a4f
Monisha <monishadass>
2025-05-04 02:40:15 +0600

    Two sum

---
+++ Two sum.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int* result = malloc(sizeof(int) * 2);
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    return NULL;
+}
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int returnSize;
+    
+    int* result = twoSum(nums, numsSize, target, &returnSize);
+
+    if (result != NULL) {
+        printf("Indices: [%d, %d]\n", result[0], result[1]);
+        free(result); // Important to avoid memory leak
+    } else {
+        printf("No two sum solution found.\n");
+    }
+
+    return 0;
+}
